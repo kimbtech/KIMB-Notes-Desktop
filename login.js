cf2ddc0bed0cb2e43c5fdca20bca7580b9ed585f
KIMB-technologies <kimbtechnology@gmail.com>
2017-10-01 21:08:55 +0200

    Icon, Menü, Loginformular und Loginversuche

--- login.js
+++ login.js
@@ -2,6 +2,102 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
-jQuery( 'button#loginsubmit' ).click(function(){
-	alert( 'sss' );
-})
+//Für die IPC Messages
+const ipc = require('electron').ipcRenderer
+// URLs testen
+var validUrl = require('valid-url');
+// Hashing
+var sjcl = require('sjcl');
+// System Dialog
+const dialog = require('electron').remote.dialog 
+
+//Loginmanager
+function mainLoginManager(){
+	//Struktur für Userdaten
+	var userdata = {
+		"server" : "",
+		"username" : "",
+		"userid" : "",
+		"authcode" : ""
+	};
+
+	//check for Login Data
+	//	sends messages to main.js to get Userinformation form there
+	//	calls next functions, sets userdata as far as possible
+	function checkForLoginData(){
+		//IPC ask for Data
+		ipc.send('ask-for-user-data')
+
+		//IPC on Data back
+		ipc.on('ask-for-user-data-back', function (event, data ) {
+			//eingeloggt?
+			if( data.loggenIn ){
+				userdata = data.userdata;
+				logUserInViaAuthcode();
+			}
+			else{
+				loginform();
+			}
+		});
+	}
+
+	//try Login
+	function logUserInViaAuthcode(){
+		alert( 'loggin in ...' + "\r\n\r\n\r\n" + JSON.stringify( userdata ) );
+	}
+
+	//loginform
+	function loginform(){
+		//zeige Formular
+		$( 'div.credentials' ).removeClass( 'disable' );
+		$( 'div.message.loading' ).addClass( 'disable' );
+
+		var password;
+		
+		//Höre auf Click
+		$( 'button#loginsubmit' ).click(function(){
+			//check User Data and get Authcode
+			
+			//aus Formular holen
+			userdata.server = $( "input#serverurl" ).val();
+			userdata.username = $( "input#username" ).val();
+			password = $( "input#password" ).val();
+
+			if(
+				validUrl.isUri(  userdata.server )
+				&&
+				userdata.username.replace( /[^a-z]/, '' ) === userdata.username && userdata.username != ''
+				&& 
+				password != ''
+			){
+				//Passwort Hashen
+				password = sjcl.codec.hex.fromBits( sjcl.hash.sha256.hash( password ));
+
+				//Authcode holen
+				/*
+					ToDo
+				*/
+					//Daten sichern
+					ipc.send( 'save-user-data', userdata );
+
+					alert( JSON.stringify( [ userdata, password ] ) );
+			}
+			else{
+				dialog.showErrorBox( 'Formulareingaben', 'Bitte füllen Sie allen Felder korrekt!' );
+			}
+		});
+	}
+
+	//open Notes
+	function openNotesTool(){
+
+	}
+
+	//Erstmal nach bekannten Daten gucken.
+	checkForLoginData();
+}
+//Logindaten suchen, Login versuchen, Notes öffnen,
+//	wenn keine Daten gefunden, Formular zeigen
+mainLoginManager();
+
+
--- main.js
+++ main.js
@@ -1,19 +1,36 @@
+//Entwicklungsmodus?
+const devMode = true;
+
 const electron = require('electron')
 // Module to control application life.
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu;
+const ipc = electron.ipcMain
+const dialog = electron.dialog;
 
 const path = require('path')
 const url = require('url')
+
+const os = require('os');
+const storage = require('electron-json-storage');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+const openAboutWindow = require('about-window').default;
+
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    minWidth: 300,
+    width: 900,
+    height: 700,
+    minHeight: 700,
+    icon: path.join(__dirname, 'assets/icons/png/64x64.png')
+  });
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -32,6 +49,138 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  //Menue erstellen
+  var menuTemplate = [];
+
+  //About vorne (Mac und andere getrennt)
+  if( process.platform === 'darwin' ){
+    menuTemplate.push(
+      {
+        label: 'KIMB-Notes-Desktop',
+        submenu: [
+          {
+            label: 'Über KIMB-Notes-Desktop',
+              click: () => openAboutWindow({
+                icon_path: path.join(__dirname, 'assets/icons/png/128x128.png'),
+                bug_report_url : 'https://github.com/kimbtech/KIMB-Notes-Desktop/issues',
+                copyright: 'copyright by KIMB-technologies 2017, distributed under terms of GPLv3',
+                homepage: 'https://github.com/kimbtech/KIMB-Notes-Desktop',
+                description: 'A desktop application for KIMB-Notes server.',
+                license: 'GPL-3.0'
+              })
+          },
+          {type: 'separator'},
+          {role: 'services', submenu: []},
+          {type: 'separator'},
+          {role: 'hide'},
+          {role: 'hideothers'},
+          {role: 'unhide'},
+          {type: 'separator'},
+          {
+            label: 'Ausloggen',
+            click: () => logUserOut()
+          },
+          {
+            label : 'Beenden',
+            role: 'quit'
+          }
+        ]
+      });
+    }
+    else{
+      menuTemplate.push(
+        {
+          label: 'KIMB-Notes-Desktop',
+          submenu: [
+            {
+              label: 'Über KIMB-Notes-Desktop',
+                click: () => openAboutWindow({
+                  icon_path: path.join(__dirname, 'assets/icons/png/128x128.png'),
+                  bug_report_url : 'https://github.com/kimbtech/KIMB-Notes-Desktop/issues',
+                  copyright: 'copyright by KIMB-technologies 2017, distributed under terms of GPLv3',
+                  homepage: 'https://github.com/kimbtech/KIMB-Notes-Desktop',
+                  description: 'A desktop application for KIMB-Notes server.',
+                  license: 'GPL-3.0'
+                })
+            },
+            {type: 'separator'},
+            {
+              label: 'Ausloggen',
+              click: () => logUserOut()
+            },
+            {
+              label : 'Beenden',
+              role: 'quit'
+            }
+          ]
+        });
+    }
+
+    //Weiteres Menü (alle OS gleich)
+    menuTemplate.push(
+      {
+        label: 'Bearbeiten',
+        submenu: [
+          {
+            label: 'Rückgängig',
+            role: 'undo'
+          },
+          {
+            label: 'Wiederholen',
+            role: 'redo'
+          },
+          {type: 'separator'},
+          {
+            label: 'Ausschneiden',
+            role: 'cut'
+          },
+          {
+            label: 'Kopieren',
+            role: 'copy'
+          },
+          {
+            label: 'Einfügen',
+            role: 'paste'
+          }
+        ]
+      });
+
+    menuTemplate.push(
+      {
+        label: 'Fenster',
+        submenu: [
+          {
+             label: 'Neu laden',
+             role: 'reload'
+          },
+          { type: 'separator' },
+          {
+            label: 'Minimieren',
+            role: 'minimize'
+          },
+          {
+            label: 'Schließen',
+            role: 'close'
+          }
+        ]
+      });
+
+    //Entwicklungsmenü
+    if( devMode ){
+      menuTemplate.push(
+        {
+          label: 'Entwicklung',
+          submenu: [
+            {role: 'forcereload'},
+            {role: 'toggledevtools'}
+          ]
+        });
+    }
+
+  //Menü laden
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 }
 
 // This method will be called when Electron has finished
@@ -58,3 +207,81 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+
+//User Ausloggen, Fenster neu starten, wieder nach Login fragen
+function logUserOut(){
+  //Userdaten löschen
+  storage.remove('NotesUser', function(error) {
+    if( error ){
+      throw error;
+    }
+
+    //Fenster schließen
+    mainWindow = null
+    //neues Fenster laden
+    createWindow();
+
+  });
+}
+
+function askForUserData( event ){
+  var userdata = {
+    "server" : "",
+    "username" : "",
+    "userid" : "",
+    "authcode" : ""
+  };
+  var loggedIn = false;
+
+  try{
+    //Speicherung als JSON-String unter Schlüssel "NotesUser"
+    storage.has( 'NotesUser', function(error, hasIt) {
+      if( error ){
+        throw error;
+      }
+
+      if( hasIt ){
+        //Daten lesen
+        storage.get( 'NotesUser' , function(error, data) {
+          if (error){
+            throw error;
+          }
+          //Daten nehemen
+          userdata = data;
+
+          answer( true );
+
+        });
+      }
+      else{
+        answer(false);
+      }
+
+    });
+
+    //zurückgeben
+    function answer(loggedIn){
+      event.sender.send('ask-for-user-data-back', {
+        "loggenIn" : loggedIn,
+        "userdata" : userdata
+      });
+    }
+
+  //Fehler fangen.
+  } catch( e ) {
+    dialog.showErrorBox( 'Kann Userinformationen nicht lesen!' , 'Fehler: "' + e.message + '"' );
+  }
+}
+
+function saveUserData( event, userdata ){
+  /*
+      ToDo
+  */
+  dialog.showErrorBox( 'Daten speichern' , JSON.stringify( userdata ) );
+}
+
+// Messages IPC
+ipc.on('ask-for-user-data', askForUserData );
+ipc.on('save-user-data', saveUserData );
+
